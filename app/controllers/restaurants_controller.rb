ca941dc10cfb93960860381be5acf8abbb169ca8
Robert Simon <robertsimonmedia@gmail.com>
2019-05-16 08:38:13 -0400

    completed all models, routes, controllers, and views... Need to finish css

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,43 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    # @reviews = Review.find_by(restaurant_id: @restaurant.id)
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(allowed_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(allowed_params)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(allowed_params)
+    # @review.restaurant = Restaurant.find(params[:review][:restaurants_id])
+    @review.save
+    redirect_to restaurant_path(@review.restaurant_id)
+  end
+
+  private
+
+  def allowed_params
+    params.require(:review).permit(:content, :rating, :restaurant_id)
+  end
 end
